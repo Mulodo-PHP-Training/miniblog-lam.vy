139d7b976f9fdfba226318255579c5156baaf13c
lam-vy-mulodo <lam.vy@mulodo.com>
2014-12-29 11:29:44 +0700

    Upload code for edit comment function

--- src/fuel/app/config/routes.php
+++ src/fuel/app/config/routes.php
@@ -24,7 +24,11 @@ return array(
 	'(:version)/users(:name)?' =>  array(array('GET', new Route('$1/user/search_user'))), 
 	//--------------------------POST--------------------------------
 	'(:version)/posts' => array(array('POST', new Route('$1/post/create')), array('GET', new Route('$1/post/all_posts'))),
+	
 	//comment
+	//edit comment
+	'(:version)/posts/:num/comments/:num' => array(array('PUT', new Route('$1/comment/update_comment'))),
+	//add comment
 	'(:version)/posts/:post_id/comments' => array(array('POST', new Route('$1/comment/create_comment'))),
 		
 		
