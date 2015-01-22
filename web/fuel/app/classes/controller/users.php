9e2da26172ba9c9c12ad91c4e9d8e8a4623d1029
lam-vy-mulodo <lam.vy@mulodo.com>
2015-01-22 10:48:47 +0700

    upload controller user for login, update info and change password

--- web/fuel/app/classes/controller/users.php
+++ web/fuel/app/classes/controller/users.php
@@ -2,6 +2,7 @@
 use Fuel\Core\Input;
 use Fuel\Core\Session;
 use Fuel\Core\Controller_Template;
+use Fuel\Core\Security;
 /**
  * Fuel is a fast, lightweight, community driven PHP5 framework.
  *
@@ -25,7 +26,7 @@ use Fuel\Core\Controller_Template;
 class Controller_Users extends Controller_Template
 {
 
-	
+	protected $filters = array('strip_tags', 'htmlentities');
 	/**
 	 * The index function, user home page
 	 *
@@ -64,11 +65,11 @@ class Controller_Users extends Controller_Template
 		if (Input::post()) {
 			
 			//set data to call api
-			$user['username'] = Input::post('username');
-			$user['password'] = Input::post('password');
-			$user['lastname'] = Input::post('lastname');
-			$user['firstname'] = Input::post('firstname');
-			$user['email'] = Input::post('email');
+			$user['username'] = Security::clean(Input::post('username'), $this->filters);
+			$user['password'] = Security::clean(Input::post('password'), $this->filters);
+			$user['lastname'] = Security::clean(Input::post('lastname'), $this->filters);
+			$user['firstname'] = Security::clean(Input::post('firstname'), $this->filters);
+			$user['email'] = Security::clean(Input::post('email'), $this->filters);
 			//call api
 			//set method, link, use curl to call
 			$method = 'POST';
@@ -133,6 +134,198 @@ class Controller_Users extends Controller_Template
 		return Response::redirect('/index');
 	}
 	
+	/**
+	 * The function login 
+	 *
+	 * @access  public
+	 * @return  Template view
+	 */
+	
+	public function action_login() {
+	
+		$data = array();
+		$this->template->title = "Login - Miniblog";
+		$this->template->set('breadcrumbs', '<li><a href="#">Users</a></li>
+				<li class="active">Login</li>', false);
+		//check submit for login
+		if (Input::post()) {
+			//get username and password
+			$user['username'] = Security::clean(Input::post('username'), $this->filters);
+			$user['password'] = Security::clean(Input::post('password'), $this->filters);
+			//call api
+			$method = 'POST';
+			$link = 'http://localhost/_blog/blog/src/v1/users/login';
+			$res = $this->init_curl($user, $method, $link);
+			//check result code return is 200
+			if ($res['meta']['code'] == 200) {
+				Session::set('token', $res['data']['token']);
+				Session::set('user_id', $res['data']['id']);
+				Session::set('username', $res['data']['username']);
+				//set the success message alert
+				$data['result'] = '<div class="alert alert-success" role="alert">
+									<strong>Success!</strong> '.$res['meta']['messages'].'
+		        				</div>';
+			} else {
+				//alert error
+				if (is_array($res['meta']['message'])) {
+					//json return is array message output
+					$message = implode($res['meta']['message'][0]);
+					//var_dump($res['meta']);die;
+				} else {
+						
+					$message = $res['meta']['message'];
+				}
+				//set the error message alert
+				$data['result'] = '<div class="alert alert-warning" role="alert">
+									<strong>Sorry!</strong> '.$message.'
+		        				</div>';
+			}
+			
+			
+		} 
+		$this->template->content = View::forge('users/login', $data);
+		
+	}
+	
+	/**
+	 * The function update info user 
+	 *
+	 * @access  public
+	 * @return  Template view
+	 */
+	
+	public function action_update() {
+		$data = array();
+		$this->template->title = "Update user - Miniblog";
+		$this->template->set('breadcrumbs', '<li><a href="#">Users</a></li>
+				<li class="active">Update</li>', false);
+		//check user logged
+		if (Session::get('token')) {
+			
+			//check if submit
+			if (Input::post()) {
+				//call api to update
+				$token = Session::get('token');
+		
+				$user = array (
+							'token' => $token ,
+							'firstname' => Security::clean(Input::post('firstname'), $this->filters),
+							'lastname' => Security::clean(Input::post('lastname'), $this->filters) ,
+							'email' => Security::clean(Input::post('email'), $this->filters),
+    
+						);
+				$method = 'PUT';
+				$link = 'http://localhost/_blog/blog/src/v1/users/';
+				//run
+				$result = $this->init_curl($user, $method, $link);
+				//sucess
+				if ($result['meta']['code'] == '200') {
+					$data['result'] = '<div class="alert alert-success" role="alert">
+									<strong>Success!</strong> Your profile was updated.
+		        				</div>';
+				} else {
+					//alert error
+					if (is_array($result['meta']['message'])) {
+						//json return is array message output
+						$message = implode($result['meta']['message'][0]);
+						//var_dump($res['meta']);die;
+					} else {
+					
+						$message = $result['meta']['message'];
+					}
+					//set the error message alert
+					$data['result'] = '<div class="alert alert-warning" role="alert">
+									<strong>Sorry!</strong> '.$message.'
+		        				</div>';
+				}
+			}
+			//get info of user want to update fill in the form
+			$user_id = Session::get('user_id');
+			$method = "GET";
+			$link = 'http://localhost/_blog/blog/src/v1/users/'.$user_id;
+			$info = $this->init_curl(null, $method, $link);
+			//fill data user info
+			$data['data'] = $info['data'];
+			
+			$this->template->content = View::forge('users/update', $data);
+		} else {
+			return Response::redirect('users/access_denied');
+		}
+	}
+	
+	/**
+	 * The function use change user password
+	 * 
+	 * @access public
+	 * @return Template view
+	 */
+	public function action_password() {
+		$data = array();
+		$this->template->title = "Change password - Miniblog";
+		$this->template->set('breadcrumbs', '<li><a href="#">Users</a></li>
+				<li class="active">Change password</li>', false);
+		if (Session::get('token')) {
+			//check submit
+			if (Input::post()) {
+				//token
+				$info['token'] = Session::get('token');
+				$info['old_password'] = Security::clean(Input::post('oldpassword'), $this->filters);
+				$info['password'] = Security::clean(Input::post('newpassword'), $this->filters);
+				$info['re'] = Security::clean(Input::post('retype'), $this->filters);
+				//check input value
+				
+				//set method
+				$method = 'PUT';
+				//add the user id into the link
+				$link = 'http://localhost/_blog/blog/src/v1/users/password';
+				$result = $this->init_curl($info, $method, $link);
+				//check the result return
+				if ($result['meta']['code'] == 200) {
+					$data['result'] = '<div class="alert alert-success" role="alert">
+								<strong>Success!</strong> Your password was changed.
+	        				</div>';
+				} else {
+					//alert error
+					if (is_array($result['meta']['message'])) {
+						//json return is array message output
+						$message = implode($result['meta']['message'][0]);
+						//var_dump($res['meta']);die;
+					} else {
+							
+						$message = $result['meta']['message'];
+					}
+					//set the error message alert
+					$data['result'] = '<div class="alert alert-warning" role="alert">
+								<strong>Sorry!</strong> '.$message.'
+	        				</div>';
+				
+				}
+			}
+			
+			//template and view change pass
+			$this->template->content = View::forge('users/password', $data);
+		} else {
+			//response access is denied
+			return Response::redirect('users/access_denied');
+		}
+	}	
+	/**
+	 * The function show error message
+	 * access is denied.
+	 *
+	 * @access  public
+	 * @return  Template view
+	 */
+	public function action_access_denied() {
+		
+		$data = array();
+		$data['result'] = '<div class="alert alert-warning" role="alert">
+									<strong>Sorry!</strong> Access is denied.
+		        				</div>';
+		$this->template->title = "Access denied - Miniblog";
+		$this->template->set('breadcrumbs','<li class="active">Error</li>', false);
+		$this->template->content = View::forge('users/access_denied', $data);
+	}
 	/**
 	 * function to init curl used to api
 	 * set method, link for request
